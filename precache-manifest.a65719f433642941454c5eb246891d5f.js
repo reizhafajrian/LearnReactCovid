f6d0275bfc1e0d376b2275bd255672080a88d917
reizhafajrian <reizha77@gmail.com>
2020-10-27 02:53:34 +0700

    Updates

---
+++ precache-manifest.a65719f433642941454c5eb246891d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cce1e3cbd3b08cdb555f85f1fa7e3e9",
+    "url": "/LearnReactCovid/index.html"
+  },
+  {
+    "revision": "40ecd44c7adf5cd8a2c5",
+    "url": "/LearnReactCovid/static/css/main.e48493ef.chunk.css"
+  },
+  {
+    "revision": "2400614324f0a0d626ed",
+    "url": "/LearnReactCovid/static/js/2.5b4668dd.chunk.js"
+  },
+  {
+    "revision": "a07000321a1ebac9c07126414b759ee0",
+    "url": "/LearnReactCovid/static/js/2.5b4668dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ecd44c7adf5cd8a2c5",
+    "url": "/LearnReactCovid/static/js/main.45fbb926.chunk.js"
+  },
+  {
+    "revision": "3e31b7edc1e1e206ea37",
+    "url": "/LearnReactCovid/static/js/runtime-main.b5a7727c.js"
+  }
+]);
